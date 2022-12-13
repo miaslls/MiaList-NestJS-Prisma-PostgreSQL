26f2db6094d719426a67cdaf2169de87d4b6f5b7
miaslls <97785569+miaslls@users.noreply.github.com>
2022-12-13 10:49:43 -0300

    feat(user): repository

---
+++ src/user/user.repository.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@nestjs/common';
+
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/database/prisma.service';
+
+import { Exception } from 'src/utils/exceptions/Exception';
+import { ExceptionType } from 'src/utils/exceptions/exception.helper';
+
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UserRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    try {
+      return this.prisma.user.create({ data });
+    } catch {
+      throw new Exception(ExceptionType.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async findAll(): Promise<User[]> {
+    try {
+      return this.prisma.user.findMany();
+    } catch {
+      throw new Exception(ExceptionType.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async findOne(username: string): Promise<User> {
+    try {
+      return this.prisma.user.findUnique({
+        where: { username },
+      });
+    } catch {
+      throw new Exception(ExceptionType.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async update(username: string, data: Prisma.UserUpdateInput): Promise<User> {
+    try {
+      const user = this.prisma.user.update({ where: { username }, data });
+      return user;
+    } catch {
+      throw new Exception(ExceptionType.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async remove(username: string): Promise<User> {
+    try {
+      return this.prisma.user.delete({ where: { username } });
+    } catch {
+      throw new Exception(ExceptionType.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
