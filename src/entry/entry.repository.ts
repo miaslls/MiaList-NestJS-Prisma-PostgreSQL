e717d7c6afd3267f7cbc81d51ae16342f974f10e
miaslls <97785569+miaslls@users.noreply.github.com>
2022-12-20 15:17:51 -0300

    feat(entry): repository

---
+++ src/entry/entry.repository.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@nestjs/common';
+
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/database/prisma.service';
+
+import { Exception } from 'src/utils/exceptions/Exception';
+import { ExceptionType } from 'src/utils/exceptions/exception.helper';
+
+import { Entry } from './entities/entry.entity';
+
+@Injectable()
+export class EntryRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: Prisma.EntryUncheckedCreateInput): Promise<Entry> {
+    try {
+      return this.prisma.entry.create({ data });
+    } catch {
+      throw new Exception(ExceptionType.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async findOne(id: string): Promise<Entry> {
+    try {
+      return this.prisma.entry.findUnique({ where: { id } });
+    } catch {
+      throw new Exception(ExceptionType.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async findOneByText(listId: string, text: string): Promise<Entry> {
+    try {
+      return this.prisma.entry.findFirst({
+        where: { listId, text },
+      });
+    } catch {
+      throw new Exception(ExceptionType.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async update(id: string, data: Prisma.EntryUncheckedUpdateInput): Promise<Entry> {
+    try {
+      return this.prisma.entry.update({ where: { id }, data });
+    } catch {
+      throw new Exception(ExceptionType.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async remove(id: string): Promise<Entry> {
+    try {
+      return this.prisma.entry.delete({ where: { id } });
+    } catch {
+      throw new Exception(ExceptionType.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
