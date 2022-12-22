ec838f633d2323f9d864f697890e75e1b77d83b5
miaslls <97785569+miaslls@users.noreply.github.com>
2022-12-22 13:20:04 -0300

    fix: examples

--- src/entry/dto/create-entry.dto.ts
+++ src/entry/dto/create-entry.dto.ts
@@ -1,18 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsMongoId, IsString } from 'class-validator';
+import { IsMongoId, IsString } from 'class-validator';
 
 export class EntryDto {
   @ApiProperty({ description: 'entry text', example: 'buy groceries' })
   @IsString()
   text: string;
 
-  @ApiProperty({ description: 'completed entry?' })
+  @ApiProperty({ description: 'completed entry?', default: false })
   completed?: boolean;
 
-  @ApiProperty({ description: 'entry date', example: 'YYYY-MM-DD' })
+  @ApiProperty({ description: 'entry date', example: '2022-12-22T00:00:00.000Z' })
   date?: Date;
 
-  @ApiProperty({ description: 'entry amount', example: '9,99' })
+  @ApiProperty({ description: 'entry amount', example: '9.99' })
   amount?: number;
 
   @ApiProperty({ description: 'entry url', example: 'http://entry.com' })
